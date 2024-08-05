171d0bea8e8e9095e6b86bceb625604a780ca838
DilyanDinev <dilqnskiq@abv.bg>
2024-08-05 11:59:23 +0300

    Update space_travel.py

--- mid_exam/space_travel.py
+++ mid_exam/space_travel.py
@@ -1,38 +1,36 @@
-sequence = input().split("||")
+travel_rout = input().split("||")
 fuel = int(input())
 ammunition = int(input())
-flag = False
-for current_travel in sequence:
-    travel = current_travel.split()
-    current_command = str(travel[0])
-    if len(agr) > 1:
-        some_numbers = int(travel[1])
-    if current_command == "Travel":
-        if fuel >= some_numbers:
-            fuel -= some_numbers
-            print(f"The spaceship travelled {some_numbers} light-years.")
-        else:
-            print("Mission failed.")
-            break
-    elif current_command == "Enemy":
-        if ammunition >= some_numbers:
-            ammunition -= some_numbers
-            print(f"An enemy with {some_numbers} armour is defeated.")
-        else:
-            fuel -= some_numbers * 2
-            if fuel >= 0:
-                print(f"An enemy with {some_numbers} armour is outmaneuvered.")
+
+for current_command in travel_rout:
+    command = current_command.split()
+    if command[0] == "Titan":
+        print("You have reached Titan, all passengers are safe.")
+        break
+    else:
+        action, number = command[0], int(command[1])
+        if action == "Travel":
+            if fuel >= number:
+                fuel -= number
+                print(f"The spaceship travelled {number} light-years.")
             else:
                 print("Mission failed.")
                 break
-    elif current_command == "Repair":
-        fuel += some_numbers
-        added_ammunition = some_numbers * 2
-        ammunition = added_ammunition
-        print(f"Ammunitions added: {added_ammunition}.")
-        print(f"Fuel added: {some_numbers}.")
-    elif current_command == "Titan":
-        flag = True
-        break
-if flag:
-    print("You have reached Titan, all passengers are safe.")
+
+        elif action == "Enemy":
+            if ammunition >= number:
+                ammunition -= number
+                print(f"An enemy with {number} armour is defeated.")
+            else:
+                fuel -= number * 2
+                if fuel >= 0:
+                    print(f"An enemy with {number} armour is outmaneuvered.")
+                else:
+                    print("Mission failed.")
+                    break
+
+        elif action == "Repair":
+            fuel += number
+            ammunition += number * 2
+            print(f"Ammunitions added: {number * 2}.")
+            print(f"Fuel added: {number}.")
